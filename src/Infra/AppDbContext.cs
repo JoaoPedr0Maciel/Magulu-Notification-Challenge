913954d9297418374aecabb5cd9f9e89d92cdcdb
João Pedro Maciel <146394493+JoaoPedr0Maciel@users.noreply.github.com>
2024-07-04 21:39:42 -0400

    feat: db Sets

--- src/Infra/AppDbContext.cs
+++ src/Infra/AppDbContext.cs
@@ -2,12 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using magalu_notify.src.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace magalu_notify.src.Infra;
 
 public class AppDbContext : DbContext
 {
+
+    public DbSet<Notification> Notifications { get; set; }
+    public DbSet<Channel> Channel { get; set; }
+    public DbSet<Status> Status { get; set; }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         const string connectionString = "Server=localhost;Database=magalu_notify;User=jpdev;Password=12345678;";
